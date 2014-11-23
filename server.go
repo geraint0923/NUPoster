d69d7edf01514f31bcaf5e82e6bc90282dbc5627
Ubuntu <nu@WildHacks.yyj.g4.internal.cloudapp.net>
2014-11-23 21:42:32 +0000

    Update code

--- server.go
+++ server.go
@@ -154,7 +154,7 @@ func main() {
 	tagMailList["lost_found"] = 26
 	tagMailList["free_food"] = 27
 
-	welcomeEmail = "<html> <div align=\"middle\"><img src=\"http://yyj.cloudapp.com:3000/welcome.jpg\"/><br/>Hi %s,<br/>Welcome to NUPoster!<br/> You can begin to post your own poster on NUPoster! Enjoy yourself!</div></html>"
+	welcomeEmail = "<html> <div align=\"middle\"><img src=\"http://yyj.cloudapp.com/welcome.jpg\"/><br/>Hi %s,<br/>Welcome to NUPoster!<br/> You can begin to post your own poster on NUPoster! Enjoy yourself!</div></html>"
 
 	m := martini.Classic()
 	m.Use(render.Renderer())
@@ -257,7 +257,7 @@ func main() {
 			_, _ = io.Copy(outputFile, file)
 		}
 		tagString := ""
-		hostUrl := "http://yyj.cloudapp.net:3000"
+		hostUrl := "http://yyj.cloudapp.net"
 		poster.Image = hostUrl + "/img/" + imgPath
 
 		tmpl, _ := template.ParseFiles("templates/newsletter.tmpl")
